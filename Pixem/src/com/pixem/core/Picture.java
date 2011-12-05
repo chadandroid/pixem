3396b96be95516b07e9ac9caf035d9c0e8a306f2
iebarron1 <lesliebarron1@gmail.com>
2011-12-05 04:27:00 +0000

    Added getCombined() method which combines the effected image with the border image and returns it.

--- Pixem/src/com/pixem/core/Picture.java
+++ Pixem/src/com/pixem/core/Picture.java
@@ -28,6 +28,9 @@ package com.pixem.core;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 
 import com.pixem.borders.Border;
 import com.pixem.effects.Effect;
@@ -63,12 +66,17 @@ public class Picture {
 		current = original.copy(Config.ARGB_8888, true);
 	}
 
-	public void draw() {
-/*		draw(current);
+	public Bitmap getCombined() {
+		// Copy effected image
+		Bitmap output = current.copy(Config.ARGB_8888, true);
 
-		if(border != null) {
-			draw(border);
-		}*/
+		Canvas canvas = new Canvas(output);
+		Rect rect = new Rect(0, 0, output.getWidth(), output.getHeight());
+		
+		// Draw border on it, I think this is how its done...
+		canvas.drawBitmap(border, rect, rect, new Paint());
+		
+		return output;
 	}
 	
 	
