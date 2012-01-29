92b0b4f30bf1b4cd626a2536bb538cb64d96a48d
saman.alvi01@gmail.com <saman.alvi01@gmail.com@29e19f06-2a11-3d83-bd5e-ce97b3329b1a>
2012-01-29 22:27:11 +0000

    -removed proguard.cfg -added permissions -tried to get saving to work

--- trunk/Pixem/src/com/pixem/core/activity/MainMenu.java
+++ trunk/Pixem/src/com/pixem/core/activity/MainMenu.java
@@ -4,7 +4,12 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
@@ -16,6 +21,9 @@ public class MainMenu extends Activity {
 	private Button loadButton, galleryButton;
 	private Button takePictureButton;
 	private Button shareButton;
+	
+	private final int GALLERY_PICTURE = 0;
+	private final int CAMERA_PICTURE = 1;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -40,16 +48,17 @@ public class MainMenu extends Activity {
 		loadButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent myIntent = new Intent(v.getContext(), Filter.class);
-                startActivityForResult(myIntent, 0);
+				Intent myIntent = new Intent(MainMenu.this, Filter.class);
+                startActivity(myIntent);
 			}
 		});
 		
 		takePictureButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				createDialog("Not Implemented.");
-			}
+	            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+	            startActivityForResult(intent, CAMERA_PICTURE);
+	        }
 		});
 		
 		shareButton.setOnClickListener(new OnClickListener() {
@@ -76,10 +85,10 @@ public class MainMenu extends Activity {
 		alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
 	        public void onClick(DialogInterface dialog, int which) {
 	            //dismiss the dialog  
+	        	
+	        	alertDialog.dismiss();
 	          }
 	      });
-		
-		
 		
 		alertDialog.setCancelable(true);
 		alertDialog.show();
@@ -98,5 +107,36 @@ public class MainMenu extends Activity {
 
 
 		System.gc();
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) { 
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if ((requestCode == CAMERA_PICTURE || requestCode == GALLERY_PICTURE) && resultCode == RESULT_OK) { 
+			Intent intent = getImageData(data);
+			startActivity(intent);
+		}
+	}
+	
+	private Intent getImageData (Intent data) { 
+		
+		Uri selectedImage = data.getData();
+		String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+		Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+		cursor.moveToFirst();
+
+		int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+		String filePath = cursor.getString(columnIndex);
+		cursor.close();
+		
+		Log.d("", "== file path is: " + filePath);
+		
+		Intent intent = new Intent().setClass(MainMenu.this, Filter.class);
+		intent.putExtra("filepath", filePath);
+		intent.putExtra("uri", selectedImage);
+		
+		return intent;
 	}
 }
